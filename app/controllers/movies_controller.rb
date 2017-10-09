10719d969d19ebf3af6b18ad3d5eb825694a297c
B Haddad <bhaddad1@uncc.edu>
2017-10-09 13:56:51 +0000

    4_7

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -3,4 +3,15 @@ class MoviesController < ApplicationController
   def index
     @movies = Movie.all
   end
+  
+  def new
+    @movie = Movie.new
+  # default: render 'new' template
+  end
+  
+  def create
+    @movie = Movie.create!(params[:movie])
+    flash[:notice] = "#{@movie.title} was successfully created."
+    redirect_to movies_path
+  end
 end
